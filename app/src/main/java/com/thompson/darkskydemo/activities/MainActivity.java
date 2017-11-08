69a267269fd17cd42703f508b9dbb7f83e1deb25
Mark Thompson <mathomps.jr@gmail.com>
2017-11-07 23:57:21 -0800

    ADDED some toasts for error conditions to notify user; UPDATED recycler view refresh

--- app/src/main/java/com/thompson/darkskydemo/activities/MainActivity.java
+++ app/src/main/java/com/thompson/darkskydemo/activities/MainActivity.java
@@ -9,10 +9,10 @@ import android.support.v4.widget.SwipeRefreshLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
-import android.util.Log;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.thompson.darkskydemo.R;
 import com.thompson.darkskydemo.UIHelper;
@@ -20,6 +20,7 @@ import com.thompson.darkskydemo.adapters.DailyDataAdapter;
 import com.thompson.darkskydemo.json_models.DarkSkyCurrentWx;
 import com.thompson.darkskydemo.json_models.DarkSkyDailyDataBlock;
 import com.thompson.darkskydemo.json_models.DarkSkyForecast;
+import com.thompson.darkskydemo.networking.ConnectivityException;
 import com.thompson.darkskydemo.networking.DarkSkyClient;
 
 import java.util.ArrayList;
@@ -36,7 +37,7 @@ public class MainActivity extends AppCompatActivity {
     public static final String KEY_STATE_LAYOUT_MANAGER = "KEY_STATE_LAYOUT_MANAGER";
 
     private RecyclerView mRecyclerView;
-    private RecyclerView.Adapter mAdapter;
+    private DailyDataAdapter mAdapter;
     private RecyclerView.LayoutManager mLayoutManager;
     private Parcelable mLayoutManagerState;
     private SwipeRefreshLayout mSwipeRefreshLayout;
@@ -46,7 +47,7 @@ public class MainActivity extends AppCompatActivity {
     private TextView wxCurrentSummaryText;
     private TextView wxCurrentTempText;
 
-    private ArrayList<DarkSkyDailyDataBlock> mDailyDataList;// = new ArrayList<>();
+    private ArrayList<DarkSkyDailyDataBlock> mDailyDataList = new ArrayList<>();
     private Location mLoc;
 
     @Override
@@ -63,6 +64,8 @@ public class MainActivity extends AppCompatActivity {
         // use a linear layout manager
         mLayoutManager = new LinearLayoutManager(this);
         mRecyclerView.setLayoutManager(mLayoutManager);
+        mAdapter = new DailyDataAdapter(mDailyDataList, mRowClickListener);
+        mRecyclerView.setAdapter(mAdapter);
 
         wxCurrentDayText = (TextView)findViewById(R.id.wx_current_day_text);
         wxCurrentDayText.setText("Now");
@@ -85,9 +88,7 @@ public class MainActivity extends AppCompatActivity {
         /*
             Force reload of recyclerview on screen rotations
          */
-        if(mDailyDataList == null) {
-            getForecast();
-        }
+        getForecast();
     }
 
     private void getForecast() {
@@ -110,22 +111,23 @@ public class MainActivity extends AppCompatActivity {
             mSwipeRefreshLayout.setRefreshing(false);
             MainActivity.this.setCurrentWx(darkSkyForecast.getDarkSkyCurrentWx());
             mDailyDataList = darkSkyForecast.getDarkSkyDailyWx().getDailyDataBlocks();
-            // specify an adapter
-            mAdapter = new DailyDataAdapter(mDailyDataList, mRowClickListener);
-            mRecyclerView.setAdapter(mAdapter);
-            mAdapter.notifyDataSetChanged();
+            mAdapter.updateDataSet(mDailyDataList);
         }
 
         @Override
         public void onHttpError(int HTTPCode, String reason) {
             mSwipeRefreshLayout.setRefreshing(false);
-            Log.d("TAG", " code:"+ HTTPCode);
+            showToast("Ugh, request error HTTP code " + HTTPCode);
         }
 
         @Override
         public void onIOFailure(Throwable t) {
             mSwipeRefreshLayout.setRefreshing(false);
-            Log.d("TAG", " failure:"+ t.getMessage());
+            if(t instanceof ConnectivityException) {
+                showToast(t.getMessage());
+            } else {
+                showToast("Ugh, unknown error " + t.getMessage());
+            }
         }
     };
 
@@ -140,5 +142,9 @@ public class MainActivity extends AppCompatActivity {
         Intent intent = new Intent(MainActivity.this, WeatherDetailActivity.class);
         intent.putExtra(KEY_STRING_DAILYBLOCK, dailyDataBlock.toString());
         startActivity(intent);
+    }
+
+    private void showToast(String msg) {
+        Toast.makeText(this, msg, Toast.LENGTH_LONG).show();
     }
 }
